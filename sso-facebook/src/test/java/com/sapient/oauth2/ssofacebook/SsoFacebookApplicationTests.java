e6fae19640ab610b31fc9d4b96249ea8ba2b40af
samarjit adhikari <samarjit.adhikari@microfocus.com>
2019-07-31 20:25:19 +0530

    Added sso-facebook

---
+++ sso-facebook/src/test/java/com/sapient/oauth2/ssofacebook/SsoFacebookApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sapient.oauth2.ssofacebook;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SsoFacebookApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
